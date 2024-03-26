311f62594d4e0461a289802ab2389106e71aeece
gourderased <gourderased@gmail.com>
2024-03-27 01:40:31 +0900

    [MA-6] feat : identification focus시 배경 변경 적용

--- presentation/src/main/java/com/motax/modutaxi/presentation/ui/intro/signup/identification/OnboardingIdentificationFragment.kt
+++ presentation/src/main/java/com/motax/modutaxi/presentation/ui/intro/signup/identification/OnboardingIdentificationFragment.kt
@@ -3,11 +3,13 @@ package com.motax.modutaxi.presentation.ui.intro.signup.identification
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.motax.modutaxi.presentation.R
 import com.motax.modutaxi.presentation.base.BaseFragment
 import com.motax.modutaxi.presentation.databinding.FragmentOnboardingIdentificationBinding
+import kotlinx.coroutines.launch
 
 class OnboardingIdentificationFragment :
     BaseFragment<FragmentOnboardingIdentificationBinding>(R.layout.fragment_onboarding_identification) {
@@ -18,17 +20,33 @@ class OnboardingIdentificationFragment :
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
         setCheckBtnListener()
-        setRadioGroupListener()
+        setListeners()
+        observeFocusedFieldWithLifeCycleScope()
     }
 
-    private fun setRadioGroupListener() {
-        binding.rgGender.setOnCheckedChangeListener{ _, checkedId ->
-            when(checkedId) {
-                R.id.radioButtonMale -> viewModel.updateGender("Male")
-                R.id.radioButtonFemale -> viewModel.updateGender("Female")
-            }
+    private fun setListeners() {
+        binding.etName.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) viewModel.focusOnName()
+        }
+
+        binding.etPhoneNumber.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) viewModel.focusOnPhoneNumber()
+        }
+
+        binding.rgGender.setOnCheckedChangeListener { _, checkedId ->
+            handleGenderSelection(checkedId)
+            viewModel.focusOnGender()
         }
     }
+
+    private fun handleGenderSelection(checkedId: Int) {
+        when (checkedId) {
+            R.id.radioButtonMale -> viewModel.updateGender("Male")
+            R.id.radioButtonFemale -> viewModel.updateGender("Female")
+        }
+    }
+
+
     private fun setCheckBtnListener() {
         binding.btnCheck.setOnClickListener {
             binding.btnCheck.isEnabled
@@ -40,5 +58,38 @@ class OnboardingIdentificationFragment :
         val action =
             OnboardingIdentificationFragmentDirections.actionIdentificationFragmentToPhoneAuthFragment()
         navigate(action)
+    }
+
+    private fun observeFocusedFieldWithLifeCycleScope() {
+        lifecycleScope.launch {
+            viewModel.focusedField.collect() { it ->
+                when (it) {
+                    OnboardingIdentificationViewModel.FocusedField.NAME -> {
+                        binding.layoutName.setBackgroundResource(R.drawable.rect_nofill_blackstroke_12radius)
+                        binding.layoutPhoneNumber.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
+                        binding.layoutGender.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
+                    }
+
+                    OnboardingIdentificationViewModel.FocusedField.PHONE -> {
+                        binding.layoutPhoneNumber.setBackgroundResource(R.drawable.rect_nofill_blackstroke_12radius)
+                        binding.layoutName.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
+                        binding.layoutGender.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
+
+                    }
+
+                    OnboardingIdentificationViewModel.FocusedField.GENDER -> {
+                        binding.layoutGender.setBackgroundResource(R.drawable.rect_nofill_blackstroke_12radius)
+                        binding.layoutName.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
+                        binding.layoutPhoneNumber.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
+
+                    }
+
+                    else -> {
+                        binding.layoutName.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
+                        binding.layoutPhoneNumber.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
+                    }
+                }
+            }
+        }
     }
 }
