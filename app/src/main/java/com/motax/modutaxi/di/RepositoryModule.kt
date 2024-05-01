480d526b2d5d614056ed062492005e471469ea94
박진성 <plashdof@gmail.com>
2024-05-01 18:50:50 +0900

    [MA-7] refactor : Di setting 변경

--- app/src/main/java/com/motax/modutaxi/di/RepositoryModule.kt
+++ app/src/main/java/com/motax/modutaxi/di/RepositoryModule.kt
@@ -1,25 +1,18 @@
 package com.motax.modutaxi.di
 
-import com.motax.modutaxi.data.remote.IntroApi
-import com.motax.modutaxi.domain.repository.IntroRepository
 import com.motax.modutaxi.data.repository.IntroRepositoryImpl
+import com.motax.modutaxi.domain.repository.IntroRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
-
-//    @Singleton
-//    @Binds
-//    abstract fun bindMyPageRepository(myPageRepositoryImpl: MyPageRepositoryImpl): MyPageRepository
+abstract class RepositoryModule {
 
     @Singleton
-    @Provides
-    fun provideIntroRepository(api: IntroApi): IntroRepository {
-        return IntroRepositoryImpl(api)
-    }
+    @Binds
+    abstract fun bindIntroRepository(introRepositoryImpl: IntroRepositoryImpl): IntroRepository
 }
