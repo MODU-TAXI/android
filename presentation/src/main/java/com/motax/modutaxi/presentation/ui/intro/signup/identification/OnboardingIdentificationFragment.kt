9d7c0acdd148d4148fe643043a1e606eaf239e54
박진성 <plashdof@gmail.com>
2024-05-02 16:46:09 +0900

    [MA-6] refactor : 회원정보 입력 화면 리팩토링

--- presentation/src/main/java/com/motax/modutaxi/presentation/ui/intro/signup/identification/OnboardingIdentificationFragment.kt
+++ presentation/src/main/java/com/motax/modutaxi/presentation/ui/intro/signup/identification/OnboardingIdentificationFragment.kt
@@ -5,14 +5,13 @@ import android.os.Bundle
 import android.telephony.PhoneNumberFormattingTextWatcher
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.motax.modutaxi.presentation.R
 import com.motax.modutaxi.presentation.base.BaseFragment
 import com.motax.modutaxi.presentation.databinding.FragmentOnboardingIdentificationBinding
-import kotlinx.coroutines.launch
 
 class OnboardingIdentificationFragment :
     BaseFragment<FragmentOnboardingIdentificationBinding>(R.layout.fragment_onboarding_identification) {
@@ -22,56 +21,62 @@ class OnboardingIdentificationFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
-        setCheckBtnListener()
+        initStateObserve()
+        initEventObserve()
         setListeners()
-        setTextChangeListener()
-        observeFocusedFieldWithLifeCycleScope()
-        requestFocusAndShowKeyboard()
+    }
 
-        binding.root.setOnClickListener {
-            hideKeyboard()
+    private fun initStateObserve() {
+        repeatOnStarted {
+            viewModel.uiState.collect {
+                when (it.focusedField) {
+                    FocusedField.NAME -> {
+                        binding.etName.requestFocus()
+                        showKeyboard(binding.etName)
+                    }
+
+                    FocusedField.PHONE -> {
+                        binding.etPhoneNumber.requestFocus()
+                        showKeyboard(binding.etPhoneNumber)
+                    }
+
+                    else -> {
+                        binding.etName.clearFocus()
+                        binding.etPhoneNumber.clearFocus()
+                        hideKeyboard()
+                    }
+                }
+            }
         }
     }
 
-    private fun setTextChangeListener() {
-        binding.etPhoneNumber.addTextChangedListener(PhoneNumberFormattingTextWatcher())
-    }
-
-    private fun requestFocusAndShowKeyboard() {
-        binding.etName.requestFocus()
-
-        val inputMethodManager =
-            context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-        inputMethodManager?.showSoftInput(binding.etName, InputMethodManager.SHOW_IMPLICIT)
+    private fun initEventObserve() {
+        repeatOnStarted {
+            viewModel.event.collect {
+                when (it) {
+                    is OnboardingIdentificationEvent.NavigateToPhoneAuth -> findNavController().toPhoneAuth()
+                }
+            }
+        }
     }
 
     private fun setListeners() {
+
+        binding.etPhoneNumber.addTextChangedListener(PhoneNumberFormattingTextWatcher())
+
+        binding.rgGender.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.radioButtonMale -> viewModel.updateGender("Male")
+                R.id.radioButtonFemale -> viewModel.updateGender("Female")
+            }
+        }
+
         binding.etName.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) viewModel.focusOnName()
+            if(hasFocus) viewModel.focusOnName()
         }
 
         binding.etPhoneNumber.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) viewModel.focusOnPhoneNumber()
-        }
-
-        binding.rgGender.setOnCheckedChangeListener { _, checkedId ->
-            handleGenderSelection(checkedId)
-            viewModel.focusOnGender()
-        }
-    }
-
-    private fun handleGenderSelection(checkedId: Int) {
-        when (checkedId) {
-            R.id.radioButtonMale -> viewModel.updateGender("Male")
-            R.id.radioButtonFemale -> viewModel.updateGender("Female")
-        }
-    }
-
-
-    private fun setCheckBtnListener() {
-        binding.btnCheck.setOnClickListener {
-            binding.btnCheck.isEnabled
-            findNavController().toPhoneAuth()
+            if(hasFocus) viewModel.focusOnPhoneNumber()
         }
     }
 
@@ -81,43 +86,16 @@ class OnboardingIdentificationFragment :
         navigate(action)
     }
 
-    private fun observeFocusedFieldWithLifeCycleScope() {//TODO 추후 셀렉터로 변경
-        lifecycleScope.launch {
-            viewModel.focusedField.collect() { it ->
-                when (it) {
-                    OnboardingIdentificationViewModel.FocusedField.NAME -> {
-                        binding.layoutName.setBackgroundResource(R.drawable.rect_nofill_blackstroke_12radius)
-                        binding.layoutPhoneNumber.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
-                        binding.layoutGender.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
-                    }
-
-                    OnboardingIdentificationViewModel.FocusedField.PHONE -> {
-                        binding.layoutPhoneNumber.setBackgroundResource(R.drawable.rect_nofill_blackstroke_12radius)
-                        binding.layoutName.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
-                        binding.layoutGender.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
-
-                    }
-
-                    OnboardingIdentificationViewModel.FocusedField.GENDER -> {
-                        binding.layoutGender.setBackgroundResource(R.drawable.rect_nofill_blackstroke_12radius)
-                        binding.layoutName.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
-                        binding.layoutPhoneNumber.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
-
-                    }
-
-                    else -> {
-                        binding.layoutName.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
-                        binding.layoutPhoneNumber.setBackgroundResource(R.drawable.rect_grey0_fill_nostroke_12radius)
-                    }
-                }
-            }
-        }
+    private fun showKeyboard(view: EditText) {
+        val inputMethodManager =
+            context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
     }
 
     private fun hideKeyboard() {
         val inputMethodManager =
             context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
         inputMethodManager?.hideSoftInputFromWindow(view?.windowToken, 0)
-
     }
+
 }
