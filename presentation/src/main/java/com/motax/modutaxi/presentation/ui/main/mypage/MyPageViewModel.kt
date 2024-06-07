f8102ddac41eee102e1cc2b3bd4d1419f8b86d26
gourderased <gourderased@gmail.com>
2024-06-07 17:40:38 +0900

    [MA-26] feat : 신고 대상 추가

--- presentation/src/main/java/com/motax/modutaxi/presentation/ui/main/mypage/MyPageViewModel.kt
+++ presentation/src/main/java/com/motax/modutaxi/presentation/ui/main/mypage/MyPageViewModel.kt
@@ -2,13 +2,22 @@ package com.motax.modutaxi.presentation.ui.main.mypage
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.motax.modutaxi.presentation.ui.main.home.HomeUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
-
+data class MyPageUiState(
+    val isReported: Boolean = false
+)
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(MyPageUiState())
+    val uiState: StateFlow<MyPageUiState> = _uiState.asStateFlow()
 
     fun onProfileImageClick() {
         Log.d("MyPageViewModel", "프로필 이미지 클릭!!!!!!!!!!")
