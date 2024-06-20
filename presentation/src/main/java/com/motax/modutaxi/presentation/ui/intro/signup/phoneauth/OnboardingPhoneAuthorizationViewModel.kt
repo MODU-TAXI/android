8011ba11136c8e3a8d632cdd3ae13df0e0043a4f
gourderased <gourderased@gmail.com>
2024-06-20 11:12:09 +0900

    [MA-31] feat : 가입 api 활성화

--- presentation/src/main/java/com/motax/modutaxi/presentation/ui/intro/signup/phoneauth/OnboardingPhoneAuthorizationViewModel.kt
+++ presentation/src/main/java/com/motax/modutaxi/presentation/ui/intro/signup/phoneauth/OnboardingPhoneAuthorizationViewModel.kt
@@ -121,22 +121,21 @@ class OnboardingPhoneAuthViewModel @Inject constructor(
 
     private fun signUp() {
         viewModelScope.launch {
-//            signUpUseCase(
-//                SignUpData.key,
-//                SignUpData.name,
-//                SignUpData.gender,
-//                SignUpData.phoneNumber,
-//                ""
-//            ).onSuccess {
-//                dataStoreManager.putAccessToken(it.tokenData.accessToken)
-//                dataStoreManager.putRefreshToken(it.tokenData.refreshToken)
-//                isSignUpSuccess.value = true
-//                _event.emit(PhoneAuthEvent.NavigateToEditNick)
-//            }.onFailure {
-//                _event.emit(PhoneAuthEvent.ShowToastMessage("회원가입 실패!"))
-//                _event.emit(PhoneAuthEvent.GoBackToInit)
-//            }
-            _event.emit(PhoneAuthEvent.NavigateToEditNick)
+            signUpUseCase(
+                SignUpData.key,
+                SignUpData.name,
+                SignUpData.gender,
+                SignUpData.phoneNumber,
+                ""
+            ).onSuccess {
+                dataStoreManager.putAccessToken(it.tokenData.accessToken)
+                dataStoreManager.putRefreshToken(it.tokenData.refreshToken)
+                isSignUpSuccess.value = true
+                _event.emit(PhoneAuthEvent.NavigateToEditNick)
+            }.onFailure {
+                _event.emit(PhoneAuthEvent.ShowToastMessage("회원가입 실패!"))
+                _event.emit(PhoneAuthEvent.GoBackToInit)
+            }
         }
     }
 
