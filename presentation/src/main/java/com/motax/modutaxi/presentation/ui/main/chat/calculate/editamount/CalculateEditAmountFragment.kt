25d73d1d66820cb3abc0a228e5c425532f6ca148
박진성 <plashdof@gmail.com>
2024-08-13 23:23:56 +0900

    [MA-34] feat : 방장 정산금액 입력 레이아웃 구현

---
+++ presentation/src/main/java/com/motax/modutaxi/presentation/ui/main/chat/calculate/editamount/CalculateEditAmountFragment.kt
@@ -0,0 +1,16 @@
+package com.motax.modutaxi.presentation.ui.main.chat.calculate.editamount
+
+import android.os.Bundle
+import android.view.View
+import com.motax.modutaxi.presentation.R
+import com.motax.modutaxi.presentation.base.BaseFragment
+import com.motax.modutaxi.presentation.databinding.FragmentCalculateEditAmountBinding
+
+class CalculateEditAmountFragment: BaseFragment<FragmentCalculateEditAmountBinding>(R.layout.fragment_calculate_edit_amount) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
