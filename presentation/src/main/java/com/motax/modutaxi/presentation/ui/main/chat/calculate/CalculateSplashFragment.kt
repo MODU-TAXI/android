6e31a93673c394fd011d6f1fd91de5ab16fc2e16
박진성 <plashdof@gmail.com>
2024-08-13 23:26:46 +0900

    [MA-34] feat : 방장 정산스플래시 -> 정산금액 입력 화면이동 구현

--- presentation/src/main/java/com/motax/modutaxi/presentation/ui/main/chat/calculate/CalculateSplashFragment.kt
+++ presentation/src/main/java/com/motax/modutaxi/presentation/ui/main/chat/calculate/CalculateSplashFragment.kt
@@ -2,14 +2,26 @@ package com.motax.modutaxi.presentation.ui.main.chat.calculate
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.motax.modutaxi.presentation.R
 import com.motax.modutaxi.presentation.base.BaseFragment
 import com.motax.modutaxi.presentation.databinding.FragmentCalculateSplashBinding
+import kotlinx.coroutines.delay
 
 class CalculateSplashFragment: BaseFragment<FragmentCalculateSplashBinding>(R.layout.fragment_calculate_splash) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        repeatOnStarted {
+            delay(2000)
+            findNavController().toCalculateEditAmount()
+        }
+    }
+
+    private fun NavController.toCalculateEditAmount(){
+        val action = CalculateSplashFragmentDirections.actionCalculateSplashFragmentToCalculateEditAmountFragment()
+        navigate(action)
     }
 }
