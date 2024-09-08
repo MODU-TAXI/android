cc01c406a107132dfd0c5815b4461e3ac522c8c4
박진성 <plashdof@gmail.com>
2024-09-09 01:08:37 +0900

    [MA-34] feat : 도착완료 예금주 변경 레이아웃 구현

---
+++ presentation/src/main/java/com/motax/modutaxi/presentation/ui/main/chat/calculate/accountowner/CalculateEditAccountOwnerFragment.kt
@@ -0,0 +1,16 @@
+package com.motax.modutaxi.presentation.ui.main.chat.calculate.accountowner
+
+import android.os.Bundle
+import android.view.View
+import com.motax.modutaxi.presentation.R
+import com.motax.modutaxi.presentation.base.BaseFragment
+import com.motax.modutaxi.presentation.databinding.FragmentCalculateEditAcocuntOwnerBinding
+
+class CalculateEditAccountOwnerFragment : BaseFragment<FragmentCalculateEditAcocuntOwnerBinding>(R.layout.fragment_calculate_edit_acocunt_owner) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
